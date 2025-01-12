6e8126679ea1385a8f48ac9a52e2fe316bdac2a3
jdmax15 <joel.maxwell15@gmail.com>
2025-01-12 11:42:21 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Main module for the project.
+"""
+
+import sys
+from functions import *
+
+def main():
+    """
+    Main entry point for the script.
+    """
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
